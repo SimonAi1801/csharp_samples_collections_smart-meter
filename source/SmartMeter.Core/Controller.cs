e7698b08630b7fe3117e11fb154566741492d9bd
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2019-10-24 19:09:13 +0200

    example code

--- source/SmartMeter.Core/Controller.cs
+++ source/SmartMeter.Core/Controller.cs
@@ -41,16 +41,57 @@ namespace SmartMeter.Core
         {
             _holidays = new Dictionary<DateTime, string>();
 
-            throw new NotImplementedException();
+            string[] lines = File.ReadAllLines(
+                Path.Combine(_inputFilePath, holidayFileName),
+                Encoding.UTF8);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(";");
+                string description = parts[0];
+                DateTime date = DateTime.Parse(parts[1]);
+
+                _holidays.Add(date, description);
+            }
         }
 
 
         private void InitMeasurements(string[] inputFileNames)
         {
             _measurements = new List<Day>();
+            Dictionary<DateTime, double> dailyMeasurments = new Dictionary<DateTime, double>();
+            foreach (string input in inputFileNames)
+            {
+                string[] lines = File.ReadAllLines(Path.Combine(_inputFilePath, input), Encoding.UTF8);
 
-            throw new NotImplementedException();
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(";");
+                    DateTime timestamp = DateTime.Parse(parts[0]);
+                    double measurment = double.Parse(parts[1]);
+
+                    if (dailyMeasurments.ContainsKey(timestamp.Date))
+                    {
+                        dailyMeasurments[timestamp.Date] += measurment;
+                    }
+                    else
+                    {
+                        dailyMeasurments[timestamp.Date] = measurment;
+                    }
+                }
+            }
+            foreach (KeyValuePair<DateTime,double> entries  in dailyMeasurments)
+            {
+                if (_holidays.ContainsKey(entries.Key))
+                {
+
+                }
+                else
+                {
+
+                }
+            }
         }
-
+        
     }
 }
